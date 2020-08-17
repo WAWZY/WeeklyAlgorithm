439944b70c8ddaf4901878189c499b8df308225f
s3cret <j.s3cret@gmail.com>
2020-08-17 16:36:57 +0800

    非负整数

---
+++ FeiFuZhengShu.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class FeiFuZhengShu {
+
+    public static void main(String[] args) {
+
+        int output = 0;
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        
+        ArrayList<Integer> input = new ArrayList<>();
+
+        for(int i = 0; i < n; i ++) {
+            input.add(sc.nextInt());
+        }
+
+        sc.close();
+        
+        // Solution
+
+        Collections.sort(input);
+
+        if(x == 0) {
+            if(input.get(0) == 0) {
+                System.out.println(1);
+            }else{
+                System.out.println(0);
+            }
+        }else {
+
+            for(int i = 0; i < input.size(); i ++) {
+
+                int temp = input.get(i);
+                if(temp < x) {
+                    output --;
+                }else if(temp == x){
+                    output ++;
+                }else {
+                    break;
+                }
+            }
+
+            System.out.println(x + output);
+
+        }
+    }
+}
